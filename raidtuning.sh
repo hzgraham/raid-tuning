aa4b3b51163864775840596f3b7f34bc1613edd7
Henry Graham <hgraham@redhat.com>
2013-07-08 23:24:38 -0400

    change to output

--- raidtuning.sh
+++ raidtuning.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-#echo 3 > /proc/sys/vm/drop_caches
 
 for i in {512..15360..512}; do
+
+echo 3 > /proc/sys/vm/drop_caches
+
 echo $i > /sys/block/md0/md/stripe_cache_size
 
-echo stripe_cache_size ${i} > ~/results.txt
+echo stripe_cache_size ${i} >> /tmp/results.txt
 
 # write
-dd if=/dev/zero of=/path/to/file bs=3355443 count=5120 >>
+dd if=/dev/zero of=/mnt/testwrite.txt bs=3355443 count=5120 >> /tmp/results.txt
+
+echo 3 > /proc/sys/vm/drop_caches
 
 # read
-dd if=/path/to/file of=/dev/null bs=3355443 count=5120
+dd if=/mnt/testread.txt of=/dev/null bs=3355443 count=5120 >> /tmp/results.txt
 
 done
-
